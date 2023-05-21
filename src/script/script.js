46c740c06703030722b07762d6573c2c6f881e6c
Сергей Голованов <multyserey@yandex.ru>
2023-05-21 21:53:27 +0500

    init

---
+++ src/script/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    const jqNext = $('.jq-next');
+    const jqPrev = $('.jq-prev');
+    const guys = $('.guy').length;
+    var index = 1;
+
+    jqNext.on('click', function () {
+        var currentImg = $('.jq-active');
+        var nextImg = currentImg.next();
+
+        if (index >= guys) {
+            jqNext.prop('disabled', true);
+        }
+        else {
+            index++;
+            jqPrev.prop('disabled', false);
+            currentImg.removeClass('jq-active');
+            nextImg.addClass('jq-active');
+        }
+        console.log(index, guys);
+    });
+    jqPrev.on('click', function () {
+        var currentImg = $('.jq-active');
+        var prevImg = currentImg.prev();
+
+        jqNext.prop('disabled', false);
+        if (index <= 1) {
+            jqPrev.prop('disabled', true);
+        }
+        else {
+            index--;
+            jqNext.prop('disabled', false);
+            currentImg.removeClass('jq-active');
+            prevImg.addClass('jq-active');
+        }
+        console.log(index, guys);
+    });
+});
